f3daec8a332f54917134a0cf3f6d271ad8c16357
Kushalkatta00 <31038786+Kushalkatta00@users.noreply.github.com>
2019-04-25 12:25:42 +0530

    Update electron.js
    Changed to Full Screen Mode

--- public/electron.js
+++ public/electron.js
@@ -12,7 +12,7 @@ const { autoUpdater } = require("electron-updater");
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({width: 900, height: 680, fullscreen: true });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
