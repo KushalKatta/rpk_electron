3293e370c4d1be2e545209543f6806617988e982
Kushal Katta <kushalkatta16@gmail.com>
2019-04-25 14:03:43 +0530

    Added Grid Format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
-import { Progress } from 'antd';
+import { Progress, Button } from 'antd';
 import 'antd/dist/antd.css';
 
 import { getDatabaseStatus } from './util/util';
 
 import PurchaseForm from './Component/PurchaseForm';
+import StockGrid from './Component/StockGrid';
 
 class App extends Component {
-  state = { loading: true }
+  state = { loading: true, formName: 'edit' }
 
   componentWillMount() {
     getDatabaseStatus().then(data => {
@@ -28,9 +29,22 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2 style={{ color: "white" }}>Welcome to RP Katta</h2>
+          <div class="stickRight">
+            <Button
+              icon="edit"
+              size="large"
+              style={{ backgroundColor: "transparent", color: 'white' }}
+              onClick={() => this.setState({ formName: 'edit' }) }
+            />
+            <Button
+              icon="table"
+              size="large"
+              style={{ backgroundColor: "transparent", color: 'white' }}
+              onClick={() => this.setState({ formName: 'table' }) }
+            />
+          </div>
         </header>
-        {/* <Comp/> */}
-        <PurchaseForm />
+        { this.state.formName === 'table' ? <StockGrid /> : <PurchaseForm /> }
       </div>
     );
   }
